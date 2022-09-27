f6c2a75487cf3c14119f89c272a2fd0139ac1972
oussema <oghawi220@gmail.com>
2022-09-27 22:39:32 +0100

    Project File

---
+++ js/mail.js
@@ -0,0 +1,16 @@
+const mailSender = () => {
+	const nameInput = document.getElementById('contactName').value;
+	const emailInput = document.getElementById('contactEmail').value;
+	const subjectInput = document.getElementById('contactSubject').value;
+	const messageInput = document.getElementById('contactMessage').value;
+
+	Email.send({
+		Host: 'smtp.elasticemail.com',
+		Username: 'oghawi220@gmail.com',
+		Password: 'D200887E76A9FFF3A15DF5709A80A051608D',
+		To: 'oghawi220@gmail.com',
+		From: emailInput,
+		Subject: subjectInput,
+		Body: 'And this is the body',
+	}).then((message) => alert(message));
+};
